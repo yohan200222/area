9a401b53dfa2242a227546397b72d95043e453ac
yohan200222 <99367178+yohan200222@users.noreply.github.com>
2022-02-16 19:57:17 -0400

    Add files via upload

---
+++ letras iguales o diferentes.py
@@ -0,0 +1,6 @@
+a=str(input("ingrese una letra:"))
+b=str(input("ingrese otra letra:"))
+if('a'=='b'):
+    print("las letras son iguales")
+    elif('a'!='b'):
+        print("las letras no son iguales")
