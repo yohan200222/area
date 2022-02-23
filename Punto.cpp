2a8d8a26ce2cd9e906c82fd8bf3feefe6b6e6c9f
yohan200222 <99367178+yohan200222@users.noreply.github.com>
2022-02-23 16:03:35 -0600

    Create Punto.cpp

---
+++ Punto.cpp
@@ -0,0 +1,15 @@
+#include "Punto.h"
+
+void Punto::setX( int valorX){
+	x=valorX;
+}
+
+void Punto:: setY( int valorY){
+	y=valorY;
+}
+		int Punto:: getX(){
+			return x;
+		}
+		int Punto:: getY(){
+			return y;
+		}
