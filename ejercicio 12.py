beaaea953901bd615d0af5608c35c6c8eecc33bc
yohan200222 <99367178+yohan200222@users.noreply.github.com>
2022-02-23 19:41:34 -0400

    Add files via upload

---
+++ ejercicio 12.py
@@ -0,0 +1,9 @@
+class Rectangulo
+def _init_(self,b,h):
+    self .b=b
+    self .h=h
+    
+def area(self):
+    return self.b*self.h
+rectangulo=Rectangulo(20,10)
+print("area del rectangulo:",rectangulo.area())
